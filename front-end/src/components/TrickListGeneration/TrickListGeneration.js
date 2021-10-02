441a3b2d893a9dd7e996c9a755112300a2b83fbd
Jacob Harris <jgharris314@gmail.com>
2021-10-02 03:57:17 -0600

    trick line is printing a semi appropriate string. we will see how it behaves as more tricks are added to the database

--- front-end/src/components/TrickListGeneration/TrickListGeneration.js
+++ front-end/src/components/TrickListGeneration/TrickListGeneration.js
@@ -6,18 +6,18 @@ const TrickListGeneration = () => {
 	const [allTricks, setAllTricks] = useState([]);
 	const [trickLine, setTrickLine] = useState("");
 
-	const [selectedDifficulty, setSelectedDifficulty] = useState(1);
+	const [selectedDifficulty, setSelectedDifficulty] = useState(0);
 	const [numberOfTricks, setNumberOfTricks] = useState(1);
-	const [tricksToConsider, setTricksToConsider] = useState([]);
+	const [tricksToConsider, setTricksToConsider] = useState(allTricks);
 	const [selectedTrickObjectState, setSelectedTrickObjectState] = useState([
 		{
-			trick_name: "little cup",
+			trick_name: "pick",
 			trick_difficulty: 0,
 			trick_grip: "candle",
 			isEnder: false,
 		},
 		{
-			trick_name: "base cup",
+			trick_name: "trix",
 			trick_difficulty: 0,
 			trick_grip: "candle",
 			isEnder: false,
@@ -29,6 +29,34 @@ const TrickListGeneration = () => {
 
 	const generateLine = () => {
 		prepTricks();
+
+		let preppedLine = "";
+		let prevId = null;
+		let prevGrip = null;
+
+		for (let i = 0; i < selectedTrickObjectState.length; i++) {
+			if (selectedTrickObjectState[i].trick_grip !== prevGrip && i > 0) {
+				preppedLine += ` trade to ${selectedTrickObjectState[i].trick_grip} ${selectedTrickObjectState[i].trick_name}`;
+			} else if (
+				selectedTrickObjectState[i].trick_id === prevId &&
+				selectedTrickObjectState[i].trick_name !== "spike"
+			) {
+				preppedLine += ` spike ${selectedTrickObjectState[i].trick_name}`;
+			} else {
+				preppedLine += `  ${selectedTrickObjectState[i].trick_name}`;
+			}
+			prevGrip = selectedTrickObjectState[i].trick_grip;
+			prevId = selectedTrickObjectState[i].trick_id;
+		}
+
+		if (
+			!selectedTrickObjectState[selectedTrickObjectState.length - 1]
+				.isEnder
+		) {
+			preppedLine += ` spike`;
+		}
+
+		setTrickLine(preppedLine);
 	};
 
 	const prepTricks = () => {
@@ -46,7 +74,6 @@ const TrickListGeneration = () => {
 		}
 
 		setSelectedTrickObjectState(selectedTrickObjects);
-		console.log(selectedTrickObjectState);
 	};
 
 	const handleDifficultyChange = (event) => {
@@ -64,19 +91,18 @@ const TrickListGeneration = () => {
 		listAllTricks(abortController.signal)
 			.then(setAllTricks)
 			.catch((error) => error);
+	}, [selectedDifficulty]);
 
-		prepTricks();
-	}, []);
+	useEffect(() => {
+		setTricksToConsider(
+			allTricks.filter(
+				(trick) => trick.trick_difficulty <= selectedDifficulty
+			)
+		);
+	}, [allTricks]);
 	return (
 		<StyledTrickListGeneration>
 			<h2>All Trix</h2>
-			<ul>
-				{allTricks.map((trick) => (
-					<li>
-						{trick.trick_name} {trick.trick_grip}
-					</li>
-				))}
-			</ul>
 			<p>Difficulty</p>
 			<select onChange={(event) => handleDifficultyChange(event)}>
 				<option value={0}>Easy</option>
@@ -102,13 +128,7 @@ const TrickListGeneration = () => {
 				))}
 			</ul>
 			<h3>Trick Line</h3>
-			<ul>
-				{selectedTrickObjectState.map((trick) => (
-					<li>
-						{trick.trick_grip} {trick.trick_name}
-					</li>
-				))}
-			</ul>
+			<p>{trickLine}</p>
 		</StyledTrickListGeneration>
 	);
 };
--- front-end/src/components/TrickListGeneration/trick-list-generation.styles.js
+++ front-end/src/components/TrickListGeneration/trick-list-generation.styles.js
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export const StyledTrickListGeneration = styled.div`
 	background-color: orange;
 	margin: 0;
+	z-index: 99;
+	overflow: scroll;
 `;
