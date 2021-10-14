f9866b495201536c4fa8a6ac66b1ab48308ef564
Jacob Harris <jgharris314@gmail.com>
2021-10-13 18:39:31 -0600

    styled open game

--- front-end/src/components/GameManagement/components/OpenGame/components/PlayOpenGame/PlayOpenGame.js
+++ front-end/src/components/GameManagement/components/OpenGame/components/PlayOpenGame/PlayOpenGame.js
@@ -17,18 +17,38 @@ const PlayOpenGame = ({ trickList }) => {
 	};
 
 	const increasePlayerOneScore = () => {
-		if (playerOneScore < 3) {
+		if (playerOneScore < 2) {
 			setPlayerOneScore(playerOneScore + 1);
 		} else {
-			initialState();
+			if (window.confirm("Confirm Player One win")) {
+				window.alert("Player One wins!");
+
+				initialState();
+			}
 		}
 	};
 
 	const increasePlayerTwoScore = () => {
-		if (playerTwoScore < 3) {
+		if (playerTwoScore < 2) {
 			setPlayerTwoScore(playerTwoScore + 1);
 		} else {
-			initialState();
+			if (window.confirm("Confirm Player Two win")) {
+				window.alert("Player Two wins!");
+
+				initialState();
+			}
+		}
+	};
+
+	const reducePlayerOneScore = () => {
+		if (playerOneScore > 0) {
+			setPlayerOneScore(playerOneScore - 1);
+		}
+	};
+
+	const reducePlayerTwoScore = () => {
+		if (playerTwoScore > 0) {
+			setPlayerTwoScore(playerTwoScore - 1);
 		}
 	};
 
@@ -50,46 +70,47 @@ const PlayOpenGame = ({ trickList }) => {
 	return (
 		<StyledPlayOpenGame>
 			<h3>{trickList.name}</h3>
-			<div className="open-game-scores">
-				<div className="open-game-scores-player">
-					Player One: {playerOneScore}
+			<div className="open-game-container">
+				<div className="open-game-scores">
+					<div className="open-game-scores-row">Player One</div>
+					<div className="open-game-scores-row">{playerOneScore}</div>
 				</div>
-				<div className="open-game-scores-player">
-					Player Two: {playerTwoScore}
+				<div className="open-game-scores">
+					<div className="open-game-scores-row">Player Two</div>
+					<div className="open-game-scores-row">{playerTwoScore}</div>
 				</div>
 			</div>
 			<div className="open-game-remaining">
-				<div className="open-game-remaining-col">
+				<div className="open-game-remaining-row">
 					Tricks until Reshuffle
 				</div>
 
-				<div className="open-game-remaining-col">
+				<div className="open-game-remaining-row">
 					{gameTrickList.length}
 				</div>
 			</div>
 			<div className="open-game-current">
-				<div className="open-game-current-col">Lace it!</div>
-				<div className="open-game-current-col">{currentTrick}</div>
+				<div className="open-game-current-row">Lace it!</div>
+				<div className="open-game-current-row" id="current-trick">
+					{currentTrick}
+				</div>
 			</div>
 
 			<div className="open-game-controls">
-				<button onClick={nextTrick}>
-					{gameTrickList.length ? "Go" : "Re-shuffle"}
-				</button>
-				<button onClick={increasePlayerOneScore}>P1</button>
-				<button onClick={increasePlayerTwoScore}>P2</button>
+				<div className="open-game-controls-col">
+					<button onClick={reducePlayerOneScore}>P1 -</button>
+					<button onClick={increasePlayerOneScore}>P1 +</button>
+				</div>
+				<div className="open-game-controls-col">
+					<button onClick={nextTrick} id="next-button">
+						{gameTrickList.length ? "Next Trick" : "Re-shuffle"}
+					</button>
+				</div>
+				<div className="open-game-controls-col">
+					<button onClick={reducePlayerTwoScore}>P2 -</button>
+					<button onClick={increasePlayerTwoScore}>P2 +</button>
+				</div>
 			</div>
-			{/* <div>
-					<h4>Been there done that!</h4>
-					{
-						<ul>
-							{" "}
-							{usedTrickList.map((t) => (
-								<li>{t}</li>
-							))}
-						</ul>
-					}
-				</div> */}
 		</StyledPlayOpenGame>
 	);
 };
--- front-end/src/components/GameManagement/components/OpenGame/components/PlayOpenGame/play-open-game.styles.js
+++ front-end/src/components/GameManagement/components/OpenGame/components/PlayOpenGame/play-open-game.styles.js
@@ -1,60 +1,106 @@
 import styled from "styled-components";
-
+import { purp, outerBackgroundColor } from "../../../../../../App.variables";
 export const StyledPlayOpenGame = styled.div`
-	background-color: yellowgreen;
+	background-color: ${purp};
+	box-shadow: 0px 0px 200px inset ${outerBackgroundColor};
 	display: flex;
 	flex-direction: column;
-	max-height: 70vh;
+	height: 70vh;
 	width: 85%;
 	margin: 0 auto;
 	border-radius: 5px;
+	color: white;
+	font-weight: bold;
 
 	.open-game {
-		&-scores {
+		&-container {
 			display: flex;
 			flex-direction: row;
-			justify-content: center;
+			text-align: center;
+		}
+		&-scores {
+			display: flex;
+			flex-direction: column;
 
-			&-player {
+			align-items: center;
+			width: 100%;
+
+			&-row {
 				display: flex;
-				flex-direction: column;
-				margin: 0 auto;
-				text-align: left;
+				flex-direction: row;
+				justify-content: center;
+				text-align: center;
+				width: 85%;
 			}
 		}
 
 		&-remaining {
 			display: flex;
-			flex-direction: row;
-			justify-content: center;
-			&-col {
+			flex-direction: column;
+			align-items: center;
+			width: 100%;
+
+			&-row {
 				display: flex;
-				flex-direction: column;
-				margin: 0 auto;
-				text-align: left;
+				flex-direction: row;
+
+				justify-content: center;
+				width: 85%;
 			}
 		}
 
 		&-current {
 			display: flex;
-			flex-direction: row;
-
-			&-col {
+			flex-direction: column;
+			margin-top: 20px;
+			&-row {
 				display: flex;
-				flex-direction: column;
+				flex-direction: row;
 				margin: 0 auto;
 				text-align: left;
+				max-width: 85%;
+				/* height: 60px; */
 			}
 		}
 
 		&-controls {
 			display: flex;
 			flex-direction: row;
-			margin: 0 auto;
+			margin: 200px auto 20px auto;
+			width: 100%;
+			justify-content: center;
 
 			button {
 				width: 100px;
+				height: 75px;
+				margin: 1px;
+				background-color: lime;
+				border: none;
+				border-radius: 5px;
+				box-shadow: 0px 0px 10px inset ${purp};
+				font-weight: bold;
+			}
+
+			&-col {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				width: 33%;
+				margin: 0 auto;
 			}
 		}
+	}
+
+	#next-button {
+		width: 100%;
+		height: 150px;
+	}
+
+	#current-trick {
+		font-size: 32px;
+		/* max-height: 40px; */
+		/* margin-bottom: 20%; */
+		background-color: rgba(255, 255, 255, 0.18);
 	}
 `;
